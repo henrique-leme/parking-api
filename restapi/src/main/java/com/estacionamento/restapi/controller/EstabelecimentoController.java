dbb752c48cfbb077f9dc0f347b4472a3fa8ee2ef
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-07-14 21:06:59 -0300

    ✨feat : estabelecimento controller metodo listAll

---
+++ restapi/src/main/java/com/estacionamento/restapi/controller/EstabelecimentoController.java
@@ -0,0 +1,34 @@
+package com.estacionamento.restapi.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estacionamento.restapi.model.estabelecimento;
+import com.estacionamento.restapi.services.EstabelecimentoService;
+
+
+@RestController
+@RequestMapping(path = "/estabelecimento")
+public class EstabelecimentoController {
+
+    @Autowired
+    private EstabelecimentoService estabelecimentoService;
+
+    @GetMapping (value = "/")
+    public List<estabelecimento> findAll() {
+        return estabelecimentoService.findAll();
+    }
+
+}
+    
+
+
