ebd684d9b779e88b8002b70c2e19fb8432d5079a
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-07-22 06:19:40 -0300

    ♻ refactor: refatoracao estabelecimento controller

--- restapi/src/main/java/com/estacionamento/restapi/controller/EstabelecimentoController.java
+++ restapi/src/main/java/com/estacionamento/restapi/controller/EstabelecimentoController.java
@@ -2,11 +2,17 @@ package com.estacionamento.restapi.controller;
 
 
 import java.util.List;
+import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+import com.estacionamento.restapi.dtos.EstabelecimentoDTO;
 import com.estacionamento.restapi.model.estabelecimento;
 import com.estacionamento.restapi.services.EstabelecimentoService;
 
@@ -20,32 +26,53 @@ public class EstabelecimentoController {
 
     //Lista todos os Estabelecimentos
     @GetMapping
-    public List<estabelecimento> findAll() {
-        return estabelecimentoService.findAll();
+    public ResponseEntity<List<estabelecimento>> findAllEstabelecimento() {
+        return ResponseEntity.status(HttpStatus.OK).body(estabelecimentoService.findAll());
     }
 
     //Busca um Estabelecimento pelo id
     @GetMapping(path = "/{id}")
-    public estabelecimento findById(@PathVariable Integer id) {
-        return estabelecimentoService.findById(id);
+    public ResponseEntity<Object> findById(@PathVariable(value = "id") Integer id) {
+        Optional<estabelecimento> estabelecimentoExists = estabelecimentoService.findById(id);
+        if(!estabelecimentoExists.isPresent()){
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Estabelecimento não encontrado"); 
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(estabelecimentoExists.get());
     }
 
     // Cria um novo Estabelecimento
     @PostMapping
-    public estabelecimento create(@RequestBody estabelecimento estabelecimento) { 
-        return estabelecimentoService.create(estabelecimento);
+    public ResponseEntity<Object> createEstabelecimento(@RequestBody @Valid EstabelecimentoDTO estabelecimentoDTO) { 
+        var estabelecimentoModel = new estabelecimento();
+        BeanUtils.copyProperties(estabelecimentoDTO, estabelecimentoModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(estabelecimentoService.create(estabelecimentoModel));
+        
     }
 
     //Deleta um Estabelecimento
     @DeleteMapping(path = "/{id}")
-    public void delete(@PathVariable Integer id) {
-        estabelecimentoService.delete(id);
+    public ResponseEntity<Object> deleteEstabelecimento(@PathVariable(value = "id") Integer id) {
+        Optional<estabelecimento> estabelecimentoExists = estabelecimentoService.findById(id);
+        if(!estabelecimentoExists.isPresent()){
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Estabelecimento não encontrado"); 
+        }
+        estabelecimentoService.delete(estabelecimentoExists.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Estabelecimento deletado com sucesso");
     }
 
     // Atualiza um Estabelecimento
     @PutMapping(path = "/{id}")
-    public estabelecimento update(@RequestBody estabelecimento Estabelecimento, @PathVariable Integer id) {
-        return estabelecimentoService.update(Estabelecimento, id);
+    public ResponseEntity<Object> updateEstabelecimento(@RequestBody @Valid EstabelecimentoDTO estabelecimentoDTO, 
+                                                        @PathVariable(value = "id") Integer id) {
+        Optional<estabelecimento> estabelecimentoExists = estabelecimentoService.findById(id);
+        if(!estabelecimentoExists.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Estabelecimento não encontrado");
+        }
+        // Recebe o estabelecimento existente
+        var estabelecimentoModel = estabelecimentoExists.get();
+        //Recebe os dados
+        BeanUtils.copyProperties(estabelecimentoDTO, estabelecimentoModel);       
+        return ResponseEntity.status(HttpStatus.OK).body(estabelecimentoService.update(estabelecimentoModel));                                                   
     }
 }
     
