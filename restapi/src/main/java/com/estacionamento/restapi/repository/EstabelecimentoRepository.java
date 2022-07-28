85d7f835d54a3fcb43c3072c87f184a5a6b03fe9
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-07-28 10:22:34 -0300

    ♻ refactor: refatoracao estabelecimento repository

--- restapi/src/main/java/com/estacionamento/restapi/repository/EstabelecimentoRepository.java
+++ restapi/src/main/java/com/estacionamento/restapi/repository/EstabelecimentoRepository.java
@@ -3,9 +3,9 @@ package com.estacionamento.restapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.estacionamento.restapi.model.estabelecimento;
+import com.estacionamento.restapi.model.Estabelecimento;
 
 @Repository
-public interface EstabelecimentoRepository extends JpaRepository<estabelecimento, Integer>{
+public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento, Integer>{
     
 }
