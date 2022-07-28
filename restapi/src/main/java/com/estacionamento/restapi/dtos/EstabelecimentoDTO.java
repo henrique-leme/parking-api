cb42cbc1231bff7b695d14e36a0dde5886e07faa
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-07-28 10:25:14 -0300

    ♻ refactor: refatoracao estabelecimentodto

--- restapi/src/main/java/com/estacionamento/restapi/dtos/EstabelecimentoDTO.java
+++ restapi/src/main/java/com/estacionamento/restapi/dtos/EstabelecimentoDTO.java
@@ -5,8 +5,9 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import lombok.Builder;
 import lombok.Data;
-
+@Builder
 @Data
 public class EstabelecimentoDTO {
    
