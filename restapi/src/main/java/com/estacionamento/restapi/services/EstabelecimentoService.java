fb34c47f959b85a87072ae293ae6ef8b5b751bc6
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-08-09 00:20:26 -0300

    ♻ refactor: update metodos estabelecimento service

--- restapi/src/main/java/com/estacionamento/restapi/services/EstabelecimentoService.java
+++ restapi/src/main/java/com/estacionamento/restapi/services/EstabelecimentoService.java
@@ -3,10 +3,11 @@ package com.estacionamento.restapi.services;
 import java.util.List;
 import java.util.Optional;
 
-import com.estacionamento.restapi.exception.EstabelecimentoNotFoundException;
-import org.hibernate.annotations.NotFound;
+import com.estacionamento.restapi.dtos.EstabelecimentoDTO;
+import com.estacionamento.restapi.exception.AlreadyExistException;
+import com.estacionamento.restapi.exception.NotFoundException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.estacionamento.restapi.model.Estabelecimento;
@@ -26,34 +27,45 @@ public class EstabelecimentoService {
         return estabelecimentoRepository.findAll();
     }
 
-    public Estabelecimento findById(Integer id) throws EstabelecimentoNotFoundException {
+    public Estabelecimento findById(Integer id) throws NotFoundException {
         Estabelecimento foundEstabelecimento = estabelecimentoRepository.findById(id)
-                .orElseThrow(() -> new EstabelecimentoNotFoundException(id));
+                .orElseThrow(() -> new NotFoundException(String.format("O Estabelecimento com id %s não foi encontrado",id)));
         return foundEstabelecimento;
     }
 
-    public Estabelecimento findByCnpj(String cnpj) throws EstabelecimentoNotFoundException{
+    public Estabelecimento findByCnpj(String cnpj) throws NotFoundException {
         Estabelecimento foundEstabelecimento = estabelecimentoRepository.findByCnpj(cnpj)
-                .orElseThrow(() -> new EstabelecimentoNotFoundException(cnpj));
+                .orElseThrow(() -> new NotFoundException(String.format("Estabelecimento com CNPJ %s não foi encontrado",cnpj)));
         return foundEstabelecimento;
     }
 
-    public Estabelecimento create(Estabelecimento estabelecimentoModel) {
+    public Estabelecimento create(EstabelecimentoDTO estabelecimentoDTO) {
+        verifyIfAlreadyRegistered(estabelecimentoDTO.getCnpj());
+        var estabelecimentoModel = new Estabelecimento();
+        BeanUtils.copyProperties(estabelecimentoDTO, estabelecimentoModel);
         return estabelecimentoRepository.save(estabelecimentoModel);
     }
 
-    public void delete(Integer id) throws EstabelecimentoNotFoundException{
+    public String delete(Integer id) throws NotFoundException {
        verifyExists(id);
        estabelecimentoRepository.deleteById(id);
+       return "Estabelecimento deletado com sucesso";
     }
 
-    public Estabelecimento update(Estabelecimento estabelecimentoModel, Integer id) throws EstabelecimentoNotFoundException {
+    public Estabelecimento update(Estabelecimento estabelecimentoModel, Integer id) throws NotFoundException {
         verifyExists(id);
         return estabelecimentoRepository.save(estabelecimentoModel);
     }
 
-       private Estabelecimento verifyExists(Integer id) throws EstabelecimentoNotFoundException {
+       private Estabelecimento verifyExists(Integer id) throws NotFoundException {
         return estabelecimentoRepository.findById(id)
-                .orElseThrow(() -> new EstabelecimentoNotFoundException(id));
+                .orElseThrow(() -> new NotFoundException(String.format("Estabelecimento com id %s não foi encontrado",id)));
+       }
+
+       private void verifyIfAlreadyRegistered(String cnpj) throws AlreadyExistException {
+           Optional<Estabelecimento> existedEstabelecimento = estabelecimentoRepository.findByCnpj(cnpj);
+           if (existedEstabelecimento.isPresent()) {
+               throw new AlreadyExistException(String.format("Estabelecimento com o CNPJ %s já foi registrado",cnpj));
+           }
        }
 }
