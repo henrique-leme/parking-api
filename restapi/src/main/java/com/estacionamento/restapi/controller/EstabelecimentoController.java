a075ca6f81d1f0b90317dc0be6b1f625c9609731
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-08-09 00:19:20 -0300

    ♻ refactor: update metodos estabelecimento controller

--- restapi/src/main/java/com/estacionamento/restapi/controller/EstabelecimentoController.java
+++ restapi/src/main/java/com/estacionamento/restapi/controller/EstabelecimentoController.java
@@ -2,11 +2,11 @@ package com.estacionamento.restapi.controller;
 
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
-import com.estacionamento.restapi.exception.EstabelecimentoNotFoundException;
+import com.estacionamento.restapi.exception.AlreadyExistException;
+import com.estacionamento.restapi.exception.NotFoundException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -33,35 +33,32 @@ public class EstabelecimentoController {
 
     //Busca um Estabelecimento pelo id
     @GetMapping(path = "/{id}")
-    public ResponseEntity<Object> findEstabelecimentoById(@PathVariable(value = "id") Integer id) throws EstabelecimentoNotFoundException {
+    public ResponseEntity<Object> findEstabelecimentoById(@PathVariable(value = "id") Integer id) throws NotFoundException {
         return ResponseEntity.status(HttpStatus.OK).body(estabelecimentoService.findById(id));
     }
 
     //Busca um Estabeelcimento pelo CNPJ
     @GetMapping(path = "/cnpj/{cnpj}")
-    public ResponseEntity<Object> findEstabelecimentoByCNPJ(@PathVariable(value = "cnpj")String cnpj)throws EstabelecimentoNotFoundException {
+    public ResponseEntity<Object> findEstabelecimentoByCNPJ(@PathVariable(value = "cnpj")String cnpj)throws NotFoundException {
         return ResponseEntity.status(HttpStatus.OK).body(estabelecimentoService.findByCnpj(cnpj));
     }
 
     // Cria um novo Estabelecimento
     @PostMapping
-    public ResponseEntity<Object> createEstabelecimento(@RequestBody @Valid EstabelecimentoDTO estabelecimentoDTO) { 
-        var estabelecimentoModel = new Estabelecimento();
-        BeanUtils.copyProperties(estabelecimentoDTO, estabelecimentoModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(estabelecimentoService.create(estabelecimentoModel));
+    public ResponseEntity<Object> createEstabelecimento(@RequestBody @Valid EstabelecimentoDTO estabelecimentoDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(estabelecimentoService.create(estabelecimentoDTO));
     }
 
     //Deleta um Estabelecimento
     @DeleteMapping(path = "/{id}")
-    public ResponseEntity<Object> deleteEstabelecimento(@PathVariable(value = "id") Integer id) throws EstabelecimentoNotFoundException {
-        estabelecimentoService.delete(id);
-        return ResponseEntity.status(HttpStatus.OK).body("Estabelecimento deletado com sucesso");
+    public ResponseEntity<String> deleteEstabelecimento(@PathVariable(value = "id") Integer id) throws NotFoundException {
+        return ResponseEntity.status(HttpStatus.OK).body(estabelecimentoService.delete(id));
     }
 
     // Atualiza um Estabelecimento
     @PutMapping(path = "/{id}")
     public ResponseEntity<Object> updateEstabelecimento(@RequestBody @Valid EstabelecimentoDTO estabelecimentoDTO,
-                                                        @PathVariable(value = "id") Integer id) throws EstabelecimentoNotFoundException{
+                                                        @PathVariable(value = "id") Integer id) throws NotFoundException {
         //Recebe o estabelecimento
        Estabelecimento estabelecimentoExists = estabelecimentoService.findById(id);
 
