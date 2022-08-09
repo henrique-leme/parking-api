e0118ec1f57a09757bbba71973280979266337df
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-08-09 00:55:41 -0300

    ♻ refactor: update controller test

--- restapi/src/test/java/com/estacionamento/restapi/controller/EstabelecimentoControllerTest.java
+++ restapi/src/test/java/com/estacionamento/restapi/controller/EstabelecimentoControllerTest.java
@@ -3,6 +3,8 @@ package com.estacionamento.restapi.controller;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import com.estacionamento.restapi.dtos.EstabelecimentoDTO;
+import com.estacionamento.restapi.exception.NotFoundException;
 import com.estacionamento.restapi.setup.EstabelecimentoDTOSetup;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,10 +21,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import javax.persistence.Id;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 public class EstabelecimentoControllerTest {
@@ -36,10 +36,10 @@ public class EstabelecimentoControllerTest {
     private static final Estabelecimento VALID_ESTABELECIMENTO = EstabelecimentoSetup.createValidEstabelecimento();
 
     @BeforeEach
-    void setUp() {
-        when(estabelecimentoService.create(ArgumentMatchers.any(Estabelecimento.class))).thenReturn(VALID_ESTABELECIMENTO);
+    void setUp() throws NotFoundException {
+        when(estabelecimentoService.create(ArgumentMatchers.any(EstabelecimentoDTO.class))).thenReturn(VALID_ESTABELECIMENTO);
         when(estabelecimentoService.findAll()).thenReturn(List.of(VALID_ESTABELECIMENTO));
-        when(estabelecimentoService.findById(ArgumentMatchers.anyInt())).thenReturn(Optional.of(VALID_ESTABELECIMENTO));
+        when(estabelecimentoService.findById(ArgumentMatchers.anyInt())).thenReturn(VALID_ESTABELECIMENTO);
     }
 
     @Test
@@ -55,6 +55,11 @@ public class EstabelecimentoControllerTest {
         assertEstabelecimentoFields(body);
     }
 
+
+    @Test
+    void testDeleteEstabelecimento() {
+
+    }
 
     @Test
     void listAll_returnListofEstabelecimentos_WhenSucessful() {
@@ -78,15 +83,23 @@ public class EstabelecimentoControllerTest {
     }
 
     @Test
-    void findById_returnEstabelecimento_WhenSucessful() {
+    void findById_returnEstabelecimento_WhenSucessful() throws NotFoundException {
         ResponseEntity<Object> estabelecimentoEntity = estabelecimentoController.findEstabelecimentoById(VALID_ESTABELECIMENTO.getId());
         Estabelecimento body = (Estabelecimento) estabelecimentoEntity.getBody();
 
         Assertions.assertThat(estabelecimentoEntity).isNotNull();
-        //Assertions.assertThat(body.getId()).isNotNull().isEqualTo(estabelecimentoEntity);
+        Assertions.assertThat(body.getId()).isNotNull().isEqualTo(1);
         Assertions.assertThat(estabelecimentoEntity.getStatusCode()).isNotNull().isEqualTo(HttpStatus.OK);
     }
 
+    @Test
+    void findById_returnNotFoundEstabelecimento_WhenErrorIsReturned() {
+
+    }
+    @Test
+    void testUpdateEstabelecimento() {
+
+    }
     private void assertEstabelecimentoFields(Estabelecimento estabelecimento) {
         Assertions.assertThat(estabelecimento.getNome()).isNotNull().isNotEmpty();
         Assertions.assertThat(estabelecimento.getCnpj()).isNotNull().isNotEmpty();
