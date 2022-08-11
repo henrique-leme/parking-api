a35f9505729df5986dbf1c1f283ffc5cd1fce637
Henrique Leme <henriqueleme.brito@outlook.com>
2022-08-11 19:08:14 -0300

    ✨feat veiculo test

---
+++ restapi/src/test/java/com/estacionamento/restapi/controller/VeiculoControllerTest.java
@@ -0,0 +1,52 @@
+package com.estacionamento.restapi.controller;
+
+import com.estacionamento.restapi.dtos.VeiculoDTO;
+import com.estacionamento.restapi.exception.NotFoundException;
+import com.estacionamento.restapi.model.Veiculo;
+import com.estacionamento.restapi.services.VeiculoService;
+import com.estacionamento.restapi.setup.VeiculoDTOSetup;
+import com.estacionamento.restapi.setup.VeiculoSetup;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class VeiculoControllerTest {
+
+    @InjectMocks
+    private VeiculoController veiculoController;
+
+    @Mock
+    private VeiculoService veiculoService;
+
+    private static final Veiculo VALID_VEICULO = VeiculoSetup.createValidVeiculo();
+
+    @BeforeEach
+    void setUp() throws NotFoundException {
+        when(veiculoService.create(ArgumentMatchers.any(VeiculoDTO.class))).thenReturn(VALID_VEICULO);
+        when(veiculoService.findAll()).thenReturn(List.of(VALID_VEICULO));
+        when(veiculoService.findById(ArgumentMatchers.anyInt())).thenReturn(VALID_VEICULO);
+        when(veiculoService.findByPlaca(ArgumentMatchers.anyString())).thenReturn(VALID_VEICULO);
+        when(veiculoService.delete(ArgumentMatchers.anyInt())).thenReturn("Veiculo deletado");
+    }
+
+    private void assertVeiculoFields(Veiculo veiculo) {
+        Assertions.assertThat(veiculo.getMarca()).isNotNull().isNotEmpty();
+        Assertions.assertThat(veiculo.getModelo()).isNotNull().isNotEmpty();
+        Assertions.assertThat(veiculo.getCor()).isNotNull().isNotEmpty();
+        Assertions.assertThat(veiculo.getPlaca()).isNotNull().isNotEmpty();
+        Assertions.assertThat(veiculo.getTipo()).isNotNull();
+    }
+}
