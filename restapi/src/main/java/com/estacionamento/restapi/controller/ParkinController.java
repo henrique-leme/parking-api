7a8e1e1c32b90598e246e57605e122f9b6e02ba9
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-08-17 00:21:10 -0300

    ✨feat: parkincontrol controller

---
+++ restapi/src/main/java/com/estacionamento/restapi/controller/ParkinController.java
@@ -0,0 +1,24 @@
+package com.estacionamento.restapi.controller;
+
+import com.estacionamento.restapi.services.ParkinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/estabelecimento")
+public class ParkinController {
+
+    @Autowired
+    private ParkinService parkinService;
+
+    @PostMapping(path = "/{estabelecimento}/entrada/{veiculo}")
+    public ResponseEntity<String> entradaDeVeiculo(@PathVariable Integer estabelecimento,
+                                                   @PathVariable Integer veiculo) {
+        return ResponseEntity.status(HttpStatus.OK).body(parkinService.entrada(estabelecimento, veiculo));
+    }
+}
