482e0260e4f03ad4e2938f1eef33e78c231e8211
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-08-19 16:14:28 -0300

    📚docs: rotas swagger

--- restapi/src/main/java/com/estacionamento/restapi/controller/VeiculoController.java
+++ restapi/src/main/java/com/estacionamento/restapi/controller/VeiculoController.java
@@ -6,6 +6,8 @@ import com.estacionamento.restapi.exception.NotFoundException;
 import com.estacionamento.restapi.model.Estabelecimento;
 import com.estacionamento.restapi.model.Veiculo;
 import com.estacionamento.restapi.services.VeiculoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +19,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/veiculo")
+@CrossOrigin(origins = "*")
+@Api(value = "API Rest Estacionamento")
 public class VeiculoController {
 
     @Autowired
@@ -24,30 +28,36 @@ public class VeiculoController {
 
     //Lista todos os veiculos
     @GetMapping
+    @ApiOperation(value = "Essa rota é responsavel por listar todos os Veículos")
     public ResponseEntity<List<Veiculo>> findAllVeiculos() {
         return ResponseEntity.status(HttpStatus.OK).body(veiculoService.findAll());
     }
 
     //Busca um Veiculo pelo id
     @GetMapping(path = "/{id}")
+    @ApiOperation(value = "Essa rota é responsavel por buscar um veículo pelo id")
     public ResponseEntity<Object> findVeiculoById(@PathVariable(value = "id") Integer id) throws NotFoundException {
         return ResponseEntity.status(HttpStatus.OK).body(veiculoService.findById(id));
     }
 
     @GetMapping(path = "/placa/{placa}")
+    @ApiOperation(value = "Essa rota é responsavel por buscar um veículo pela placa")
     public ResponseEntity<Object> findVeiculoByPlaca(@PathVariable(value = "placa")String placa)throws NotFoundException {
         return ResponseEntity.status(HttpStatus.OK).body(veiculoService.findByPlaca(placa));
     }
 
     @PostMapping
+    @ApiOperation(value = "Essa rota é responsavel por criar um veículo")
     public ResponseEntity<Object> createVeiculo(@Valid @RequestBody VeiculoDTO veiculoDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(veiculoService.create(veiculoDTO));
     }
 @DeleteMapping(path = "/{id}")
+@ApiOperation(value = "Essa rota é responsavel por deletar um veículo")
     public ResponseEntity<String> deleteVeiculo(@PathVariable(value = "id") Integer id){
         return ResponseEntity.status(HttpStatus.OK).body(veiculoService.delete(id));
     }
     @PutMapping(path = "/{id}")
+    @ApiOperation(value = "Essa rota é responsavel por atualizar um veículo")
     public ResponseEntity<Object> updateVeiculo(@RequestBody @Valid VeiculoDTO veiculoDTO,
                                                         @PathVariable(value = "id") Integer id) throws NotFoundException {
         //Recebe o veiculo existente
